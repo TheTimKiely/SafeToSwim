287be7124b9c5fb34cef4067b8b12a0f221b1c3e
Tim Kiely <thetimkiely@gmail.com>
2018-03-09 18:33:04 -0500

    v0.1 of 'bloom' or 'not-bloom' is working!

--- safetoswim/clients/post.bat
+++ safetoswim/clients/post.bat
@@ -1,1 +1,1 @@
-curl -X POST -F image=@images\dog2.jpg "http://localhost:5000/predict"
+curl -X POST -F image=@images\pug.jpg "http://localhost:5000/predict"
--- safetoswim/servers/flask_server.py
+++ safetoswim/servers/flask_server.py
@@ -1,6 +1,7 @@
 from keras.applications import ResNet50
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
+from keras import models
 from PIL import Image
 import numpy as np
 import flask
@@ -8,7 +9,8 @@ import io
 
 def load_model():
     global model
-    model = ResNet50(weights="imagenet")
+    #model = ResNet50(weights="imagenet")
+    model = models.load_model('D:\code\ML\SafeToSwim\models\hab_MathBinaryClassifier.h5')
 
 def prepare_image(image, img_size):
     if image.mode != 'RGB':
@@ -39,20 +41,26 @@ def predict():
             image = Image.open(io.BytesIO(image))
 
             # preprocess the image and prepare it for classification
-            image = prepare_image(image, img_size=(224, 224))
+            image = prepare_image(image, img_size=(128, 128))
 
             # classify the input image and then initialize the list
             # of predictions to return to the client
             preds = model.predict(image)
-            results = imagenet_utils.decode_predictions(preds)
-            data["predictions"] = []
+            #pred_class = model.p
+            #results = imagenet_utils.decode_predictions(preds)
+            if preds[0][0] >= 0.5:
+                data["prediction"] = 'bloom'
+            else:
+                data["prediction"] = 'not-bloom'
 
             # loop over the results and add them to the list of
             # returned predictions
+            '''
             for (imagenetID, label, prob) in results[0]:
                 r = {"label": label, "probability": float(prob)}
                 data["predictions"].append(r)
-
+            '''
+            
             # indicate that the request was a success
             data["success"] = True
 
