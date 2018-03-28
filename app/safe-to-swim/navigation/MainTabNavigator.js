be5b678a7b7ed8c1a808dd934cfb4a289caa0c72
John Need <john.need@gmail.com>
2018-03-28 16:55:48 -0400

    changed icon

--- app/safe-to-swim/navigation/MainTabNavigator.js
+++ app/safe-to-swim/navigation/MainTabNavigator.js
@@ -9,50 +9,47 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
-export default TabNavigator(
-  {
+export default TabNavigator({
     Home: {
-      screen: HomeScreen,
+        screen: HomeScreen,
     },
     Links: {
-      screen: LinksScreen,
+        screen: LinksScreen,
     },
     Settings: {
-      screen: SettingsScreen,
+        screen: SettingsScreen,
     },
-  },
-  {
+}, {
     navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        switch (routeName) {
-          case 'Home':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-information-circle${focused ? '' : '-outline'}`
-                : 'md-information-circle';
-            break;
-          case 'Links':
-            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
-            break;
-          case 'Settings':
-            iconName =
-              Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
-        }
-        return (
-          <Ionicons
-            name={iconName}
-            size={28}
-            style={{ marginBottom: -3 }}
-            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-          />
-        );
-      },
+        tabBarIcon: ({ focused }) => {
+            const { routeName } = navigation.state;
+            let iconName;
+            switch (routeName) {
+                case 'Home':
+                    iconName =
+                        Platform.OS === 'ios' ?
+                        `ios-information-circle${focused ? '' : '-outline'}` :
+                        'md-information-circle';
+                    break;
+                case 'Links':
+                    iconName = Platform.OS === 'ios' ? `ios-camera${focused ? '' : '-outline'}` : 'md-camera';
+                    break;
+                case 'Settings':
+                    iconName =
+                        Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+            }
+            return ( <
+                Ionicons name = { iconName }
+                size = { 28 }
+                style = {
+                    { marginBottom: -3 } }
+                color = { focused ? Colors.tabIconSelected : Colors.tabIconDefault }
+                />
+            );
+        },
     }),
     tabBarComponent: TabBarBottom,
     tabBarPosition: 'bottom',
     animationEnabled: false,
     swipeEnabled: false,
-  }
-);
+});
